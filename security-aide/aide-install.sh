7126952659b62d6dfa0ab2ef9dfca6c5e55688ad
mt46 <>
2019-10-04 19:42:13 +0800

    change email subject quote

--- security-aide/aide-install.sh
+++ security-aide/aide-install.sh
@@ -16,7 +16,7 @@ echo "/var/www/html NORMAL" >> /etc/aide.conf
 
 # Create a schedule for regular check on Daily 9am
 echo "0 9 * * * root /usr/sbin/aide -c /etc/aide.conf --check 2>&1 \
-| mail -s "$EMAIL_SUBJECT" -S from=$EMAIL_SENDER $EMAIL_RECIPIENT" >> /etc/cron.d/aide
+| mail -s '$EMAIL_SUBJECT' -S from=$EMAIL_SENDER $EMAIL_RECIPIENT" >> /etc/cron.d/aide
 
 # Create initial DB
 /usr/sbin/aide -c /etc/aide.conf --init
